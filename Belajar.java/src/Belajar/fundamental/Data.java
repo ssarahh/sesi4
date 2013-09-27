8412a8baf0b7d93910dd6b9a1a27b12378123f63
ssarahh <sitisarahsuryaningtyas@gmail.com>
2013-09-27 14:42:52 +0700

    belajar.java

---
+++ Belajar.java/src/Belajar/fundamental/Data.java
@@ -0,0 +1,15 @@
+package Belajar.fundamental;
+
+public class Data {
+    private String nama; 
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    
+}
---
+++ Belajar.java/src/Belajar/fundamental/Person.java
@@ -0,0 +1,13 @@
+package Belajar.fundamental;
+
+public class Person {
+    public static void main(String[] abc){
+      Data isi = new Data();    
+      isi.setNama("Sarah");
+      byte x = 100;
+      short y = 50;
+      
+      System.out.println("Nama saya adalah "+isi.getNama());
+      System.out.println("100 + 50 = "+ (x +y));
+    }   
+}
