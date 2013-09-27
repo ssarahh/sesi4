d6c9a7c43be8458f8ba75a73e2cc4ec071ae713a
ssarahh <sitisarahsuryaningtyas@gmail.com>
2013-09-27 14:59:44 +0700

    calculator

---
+++ Calculator/src/Belajar/calculator/Data.java
@@ -0,0 +1,23 @@
+package Belajar.calculator;
+
+public class Data {
+   private int x,y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+   
+    
+}
---
+++ Calculator/src/Belajar/calculator/Person.java
@@ -0,0 +1,13 @@
+
+package Belajar.calculator;
+
+public class Person {
+    public static void main(String[] abc){
+      Data isi = new Data();
+      isi.setX(6);
+      isi.setY(10);
+      
+        System.out.println("6 + 10 = "+(isi.getX()+isi.getY()));
+        System.out.println("10 - 6 = "+(isi.getX()+isi.getY()));
+    }
+}
